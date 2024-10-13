19dfecc6e5a5be5a5c81bd970b15f0d536bca75c
z0tedd <57530789+z0tedd@users.noreply.github.com>
2024-10-13 19:07:17 +0300

    Add files via upload

--- script.py
+++ script.py
@@ -1,23 +1,3 @@
-# Парсинг подписок профиля ВК
-
-## Первое, что необходимо - запросить архив со своими данными
-
-https://vk.com/faq18145
-
-![image](https://gist.github.com/user-attachments/assets/5ca56bce-539e-49f8-8f65-6dbc119cf7ee)
-![image](https://gist.github.com/user-attachments/assets/7622e43e-e23b-4310-8a37-b54c3af87220)
-
-Спустя несколько дней вам придет сообщение о том, что архив подготовлен и его можно скачать
-![image](https://gist.github.com/user-attachments/assets/c4224b41-ff0a-4f2b-aa6b-a4af14720fb8)
-
-Открываем архив, заходим в папку profile, видим html файлы с названием subscription0. Их мы и будем парсить.
-
-![image](https://gist.github.com/user-attachments/assets/9b5e8e8a-3ccd-4710-8f04-635b91f689be)
-![image](https://gist.github.com/user-attachments/assets/9d2a5cd4-4fbe-497d-ab02-d00d87d43743)
-
-Код для обработки(кидаем его в папку profile, меняем название файла и запускаем):
-
-```python
 import os
 from chardet import detect
 from bs4 import BeautifulSoup
@@ -63,8 +43,3 @@ ResultForCSV = [
 ]
 GetCSV(ResultForCSV)
 File1.close()
-
-```
-
-Получаем profile.csv, который читается екселем.
-![image](https://gist.github.com/user-attachments/assets/8d0cdda8-0f0c-4ca0-a002-276027a6b26d)
